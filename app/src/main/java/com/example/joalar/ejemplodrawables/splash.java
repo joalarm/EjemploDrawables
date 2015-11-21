fb22e103fa7f3054d7e92e20c716522dc30d0ee7
Jose Alejandro Arias <jama0501@gmail.com>
2015-11-21 12:34:14 -0500

    Splash screen

---
+++ app/src/main/java/com/example/joalar/ejemplodrawables/splash.java
@@ -0,0 +1,15 @@
+package com.example.joalar.ejemplodrawables;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Joalar on 21/11/2015.
+ */
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
