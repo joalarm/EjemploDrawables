7a81c72b318b96b650beac58e417b00fd7a6b76d
Jose Alejandro Arias <jama0501@gmail.com>
2015-11-28 09:19:54 -0500

    Splash screen finalizado

--- app/src/main/java/com/example/joalar/ejemplodrawables/splash.java
+++ app/src/main/java/com/example/joalar/ejemplodrawables/splash.java
@@ -1,15 +1,33 @@
 package com.example.joalar.ejemplodrawables;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Joalar on 21/11/2015.
  */
 public class splash extends Activity {
+    private static final long SPLASH_SCREEN_DELAY = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(
+                        splash.this,MainActivity.class);
+            startActivity(mainIntent);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 }
